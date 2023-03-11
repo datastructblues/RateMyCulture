ea42a7c0fa0bdedbb2086e0f529bea5a1c8110c0
Umut Fidan <contact.umutfidan@gmail.com>
2023-03-12 01:19:56 +0300

    base commit.

--- app/src/main/java/com/example/ratemyculture/feature/signin/SignInActivity.kt
+++ app/src/main/java/com/example/ratemyculture/feature/signin/SignInActivity.kt
@@ -1,24 +1,28 @@
 package com.example.ratemyculture.feature.signin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.activity.viewModels
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.example.ratemyculture.BR
 import com.example.ratemyculture.R
+import com.example.ratemyculture.core.base.BaseActivity
+import com.example.ratemyculture.core.base.BaseNavigator
 import com.example.ratemyculture.databinding.ActivitySignInBinding
 import com.example.ratemyculture.feature.signup.SignUpActivity
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity<ActivitySignInBinding, SignInVM>(), BaseNavigator {
 
-    private  val viewModel: SignInVM by viewModels()
-    private lateinit var mBinding: ActivitySignInBinding
+    override val bindingVariable: Int
+        get() = BR.viewModel
+    override val layoutId: Int
+        get() = R.layout.activity_sign_in
+    override val viewModel: SignInVM = SignInVM()
+    private var mBinding: ActivitySignInBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
-        mBinding.viewModel = viewModel
+        mBinding = super.viewDataBinding
+        viewModel.navigator = this
 
     }
 
--- app/src/main/java/com/example/ratemyculture/feature/signin/SignInVM.kt
+++ app/src/main/java/com/example/ratemyculture/feature/signin/SignInVM.kt
@@ -1,20 +1,42 @@
 package com.example.ratemyculture.feature.signin
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.core.content.ContextCompat.startActivity
+import android.view.View
 import androidx.databinding.ObservableField
-import androidx.lifecycle.ViewModel
+import com.example.ratemyculture.core.base.BaseNavigator
+import com.example.ratemyculture.core.base.BaseViewModel
 import com.example.ratemyculture.feature.signup.SignUpActivity
 
-class SignInVM : ViewModel() {
+class SignInVM : BaseViewModel<BaseNavigator>() {
 
     var email = ObservableField<String>("")
     var password = ObservableField<String>("")
 
-
-    fun credentialLogin(){
-
+    fun credentialLogin() {
+        println(email.get())
+        val email = email.get()!!
+        val password = password.get()!!
+        if (email.isNullOrEmpty() || password.isNullOrEmpty()) {
+            navigator?.showAlert(
+                "Error",
+                "Please enter email and password",
+                "OK"
+            ) { dialog, _ -> dialog.dismiss() }
+        }
     }
 
+     fun checkIntentData(): Intent? {
+        navigator?.getContext()?.let {
+            return Intent(it, SignUpActivity::class.java)
+        }
+        return null
+    }
 
+    fun startSignUpActivity(){
+        println("SignInActivity.startSignUpActivity")
+        checkIntentData()?.let {
+            navigator?.openActivity(it, false)
+        }
+    }
 }
