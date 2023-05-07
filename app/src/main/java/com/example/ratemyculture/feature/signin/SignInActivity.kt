956b147f9abe134ff34b865f9337b869e6042dd9
Umut Fidan <contact.umutfidan@gmail.com>
2023-05-07 17:45:41 +0300

    permission on start, maps activity deleted. apps crashing in this commit.

--- app/src/main/java/com/example/ratemyculture/feature/signin/SignInActivity.kt
+++ app/src/main/java/com/example/ratemyculture/feature/signin/SignInActivity.kt
@@ -1,14 +1,25 @@
 package com.example.ratemyculture.feature.signin
 
+import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.ratemyculture.BR
 import com.example.ratemyculture.R
 import com.example.ratemyculture.core.base.BaseActivity
 import com.example.ratemyculture.core.base.BaseNavigator
 import com.example.ratemyculture.databinding.ActivitySignInBinding
+import com.example.ratemyculture.util.openAppSystemSettings
+import com.google.firebase.FirebaseApp
 
 
 class SignInActivity : BaseActivity<ActivitySignInBinding, SignInVM>(), BaseNavigator {
@@ -22,14 +33,22 @@ class SignInActivity : BaseActivity<ActivitySignInBinding, SignInVM>(), BaseNavi
 
     private val RC_SIGN_IN = 123
 
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = super.viewDataBinding
         viewModel.navigator = this
         viewModel.init()
+        FirebaseApp.initializeApp(this);
+     //   verifyPermissions()
     }
 
-    fun signInWithGoogle(view:View){
+    fun signInWithGoogle(view: View) {
         val signInIntent = viewModel.mGoogleSignInClient.signInIntent
         openActivityForResult(signInIntent, RC_SIGN_IN, false)
     }
@@ -40,6 +59,34 @@ class SignInActivity : BaseActivity<ActivitySignInBinding, SignInVM>(), BaseNavi
             viewModel.handleGoogleSignInResult(data)
         }
     }
+
+         private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+                permissions ->
+                val granted = permissions.all { it.value }
+                if (granted) {
+                    // Permission granted, do something here
+                } else{
+                    showAlert(
+                        "",
+                        "Please go to settings and enable permissions",
+                        "OK"
+                    ) { _, _ ->
+                        openAppSystemSettings()
+                    }
+                }
+         }
+
+
+    private fun verifyPermissions(){
+        val hasPermission = PERMISSIONS.all {
+            ActivityCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+        if (hasPermission) {
+            requestPermissionLauncher.launch(PERMISSIONS)
+        }
+    }
+
+
 
     // onStart metodu, kullanıcı oturum açmış mı kontrolü yapılır
 
@@ -56,10 +103,11 @@ class SignInActivity : BaseActivity<ActivitySignInBinding, SignInVM>(), BaseNavi
     }
 
      */
-
+    /*
     // onDestroy metodu, gereksiz nesnelerin temizlenmesi
     override fun onDestroy() {
         super.onDestroy()
     // todo bu kod yuzunden kullanıcı datalarını kaybediyordun viewModel.cleanUp()
     }
+ */
 }
--- app/src/main/java/com/example/ratemyculture/util/Extension.kt
+++ app/src/main/java/com/example/ratemyculture/util/Extension.kt
@@ -1,8 +1,11 @@
 package com.example.ratemyculture.util
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.net.Uri
+import android.provider.Settings
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -16,4 +19,11 @@ fun MarkerOptions.drawableToBitmapDescriptor(drawable: Int,context: Context): Bi
     vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
     vectorDrawable.draw(canvas)
     return BitmapDescriptorFactory.fromBitmap(bitmap)
+}
+
+fun Context.openAppSystemSettings() {
+    startActivity(Intent().apply {
+        action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        data = Uri.fromParts("package", packageName, null)
+    })
 }
